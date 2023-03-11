991b7d5f4ca9418669fe3a73701eea69a5b96591
ricky romero <riromero@ea.com>
2023-03-10 23:57:46 -0800

    [ios] link shared rust

---
+++ iOS/iOS/iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp.swift
+//  iOS
+//
+//  Created by Ricky Romero on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
