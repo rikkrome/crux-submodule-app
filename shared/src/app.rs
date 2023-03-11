7b0faf5fbcf6387b32e9757ec9ca35c5da68eaba
ricky romero <riromero@ea.com>
2023-03-10 23:20:26 -0800

    shared app file

---
+++ shared/src/app.rs
@@ -0,0 +1,67 @@
+use crux_core::{render::Render, App};
+use crux_macros::Effect;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub enum Event {
+    None,
+}
+
+#[derive(Default)]
+pub struct Model;
+
+#[derive(Serialize, Deserialize)]
+pub struct ViewModel {
+    data: String,
+}
+
+#[derive(Effect)]
+#[effect(app = "AppRoot")]
+pub struct Capabilities {
+    render: Render<Event>,
+}
+
+#[derive(Default)]
+pub struct AppRoot;
+
+impl App for AppRoot {
+    type Event = Event;
+    type Model = Model;
+    type ViewModel = ViewModel;
+    type Capabilities = Capabilities;
+
+    fn update(&self, _event: Self::Event, _model: &mut Self::Model, caps: &Self::Capabilities) {
+        caps.render.render();
+    }
+
+    fn view(&self, _model: &Self::Model) -> Self::ViewModel {
+        ViewModel {
+            data: "Hello World".to_string(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crux_core::{render::RenderOperation, testing::AppTester};
+
+    #[test]
+    fn hello_says_hello_world() {
+        let app = AppTester::<AppRoot, _>::default();
+        let mut model = Model::default();
+
+        // Call 'update' and request effects
+        let update = app.update(Event::None, &mut model);
+
+        // Check update asked us to `Render`
+        let actual_effect = &update.effects[0];
+        let expected_effect = &Effect::Render(RenderOperation);
+        assert_eq!(actual_effect, expected_effect);
+
+        // Make sure the view matches our expectations
+        let actual_view = &app.view(&model).data;
+        let expected_view = "Hello World";
+        assert_eq!(actual_view, expected_view);
+    }
+}
--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,14 +1,30 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub mod app;
+
+use lazy_static::lazy_static;
+use wasm_bindgen::prelude::wasm_bindgen;
+
+use crux_core::Core;
+pub use crux_core::Request;
+
+pub use app::*;
+
+uniffi::include_scaffolding!("shared");
+
+lazy_static! {
+    static ref CORE: Core<Effect, AppRoot> = Core::new::<Capabilities>();
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[wasm_bindgen]
+pub fn process_event(data: &[u8]) -> Vec<u8> {
+    CORE.process_event(data)
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[wasm_bindgen]
+pub fn handle_response(uuid: &[u8], data: &[u8]) -> Vec<u8> {
+    CORE.handle_response(uuid, data)
+}
+
+#[wasm_bindgen]
+pub fn view() -> Vec<u8> {
+    CORE.view()
 }
